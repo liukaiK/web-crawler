e8bf82168b2b557f7eaa514e5bd2fa5254ded2f0
liukaiK <zhizhufan@foxmail.com>
2016-05-23 10:05:59 +0800

    航旅指南

--- src/main/webapp/web/etc/js/bszn.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$('#ztff ul li').each(function(){
-		$(this).children('a').remove();
-	})
-	
-	var title = $("#NewsShowConter_nav div a:last").text();
-	$(document).attr("title", title);
-	
-})
