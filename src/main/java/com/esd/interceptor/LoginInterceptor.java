ad0144d7128b80dcc4dc21b968f2a428ec0d56b2
liukaiK <zhizhufan@foxmail.com>
2016-09-13 15:45:59 +0800

    修改拦截器功能

--- src/main/java/com/esd/interceptor/LoginInterceptor.java
+++ src/main/java/com/esd/interceptor/LoginInterceptor.java
@@ -1,6 +1,12 @@
+/*
+ * Copyright (c) 2013 哈尔滨亿时代数码科技开发有限公司（www.hrbesd.com）. All rights reserved.
+ * 
+ * HRBESD PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
 package com.esd.interceptor;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,6 +16,12 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.esd.config.BaseConfig;
 
+/**
+ * 用户登陆过滤器
+ * 
+ * @author liukaiK
+ * 
+ */
 public class LoginInterceptor implements HandlerInterceptor {
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object arg2, Exception arg3) throws Exception {
 
@@ -23,7 +35,16 @@ public class LoginInterceptor implements HandlerInterceptor {
 			Object obj = request.getSession().getAttribute(BaseConfig.USER);
 			if (obj == null) {
 				try {
-					response.sendRedirect(request.getContextPath() + "/iac/login");
+	                response.setContentType("text/html");  
+	                response.setCharacterEncoding("utf-8");  
+	                PrintWriter out = response.getWriter();    
+	                StringBuilder builder = new StringBuilder();    
+	                builder.append("<script type=\"text/javascript\" charset=\"UTF-8\">");    
+	                builder.append("alert(\"请重新登陆！\");");    
+	                builder.append("window.location.href=\"/iac/login\"");    
+	                builder.append("</script>");    
+	                out.print(builder.toString());    
+	                out.close();   
 					return false;
 				} catch (IOException e) {
 					e.printStackTrace();
